9b43879c0ba3dc785d1bca87ff999775334bc023
sw08 <yswysw0518@gmail.com>
2023-08-15 18:36:41 +0900

    Feat: Add post auto-upload code

--- upload.py
+++ upload.py
@@ -1,10 +1,16 @@
-# from github import Github, Auth
-# import json
-
-# with open('config.json') as f:
-#     config = json.load(f)
-
-# g = Github(auth=Auth.Token(config['gToken']))
-
-# sw08 = g.get_user().get_repo('sw08')
-# sw08.create_git_commit()
+from distutils.dir_util import copy_tree
+import os
+import shutil
+os.chdir("../")
+copy_tree('blog-builder/css', 'build/css')
+copy_tree('blog-builder/js', 'build/js')
+copy_tree('blog-builder/assets', 'build/assets')
+for i in os.listdir('blog-builder/pages'):
+    if os.path.isfile('blog-builder/pages/' + i):
+        shutil.copy('blog-builder/pages/' + i, 'build/'+ i)
+    else:
+        copy_tree('blog-builder/pages/' + i, 'build/'+ i)
+os.chdir('build')
+os.system('git add .')
+os.system(f'git commit -m "Docs: Upload new posts"')
+os.system('git push')

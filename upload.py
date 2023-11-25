f01160d3d272bf779a721a0ad74d2d3082e93a7f
sw08 <yswysw0518@gmail.com>
2023-11-25 23:05:23 +0900

    Fix: Edit auto-upload code

--- upload.py
+++ upload.py
@@ -1,16 +1,14 @@
 from distutils.dir_util import copy_tree
 import os
 import shutil
+
 os.chdir("../")
-copy_tree('blog-builder/css', 'build/css')
-copy_tree('blog-builder/js', 'build/js')
-copy_tree('blog-builder/assets', 'build/assets')
-for i in os.listdir('blog-builder/pages'):
-    if os.path.isfile('blog-builder/pages/' + i):
-        shutil.copy('blog-builder/pages/' + i, 'build/'+ i)
-    else:
-        copy_tree('blog-builder/pages/' + i, 'build/'+ i)
-os.chdir('build')
-os.system('git add .')
+for i in ["css", "js", "assets"]:
+    if os.path.isdir("build/" + i):
+        shutil.rmtree("build/" + i)
+    copy_tree("blog-builder/" + i, "build/" + i)
+copy_tree("blog-builder/pages", "build")
+os.chdir("./build")
+os.system("git add .")
 os.system(f'git commit -m "Docs: Update blog"')
-os.system('git push')
+os.system("git push")

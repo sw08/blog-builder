dfd842b605378dacf96e80eb3cb9bec44cc8a403
sw08 <yswysw0518@gmail.com>
2023-12-10 09:59:15 +0900

    Fix: Edit index of slicing recent posts

--- build.js
+++ build.js
@@ -125,7 +125,7 @@ for (var i = 0; i < posts.length; i++) {
 const recentPosts = [];
 var preview, date;
 const length = 300;
-for (const post of posts.length < 3 ? posts : posts.slice(3)) {
+for (const post of posts.length < 3 ? posts : posts.slice(0, 3)) {
     md.render(fs.readFileSync(`post/${categories[post]}/${post}.md`, 'utf8'));
     preview = removeTags(md.plainText.replace('\n', ' '));
     date = post.slice(0, 6);

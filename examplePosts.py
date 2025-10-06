f5d47339207f178e8a98048c978db592597b518c
Seungwoo Yi <yswysw0518@gmail.com>
2025-10-06 10:15:58 +0900

    d

--- examplePosts.py
+++ examplePosts.py
@@ -1,12 +1,16 @@
 import os
-os.chdir('post')
+
+os.chdir("post")
 import asyncio
 import aiofiles
+
+
 async def main():
     for i in range(3):
-        os.mkdir(f'category {i}')
+        os.mkdir(f"category {i}")
         for j in range(10):
-            async with aiofiles.open(f'category {i}/2001010{i}0{j}-{j}.md', 'w') as f:
-                await f.write(f'post 2001010{i}0{j}-{j}.md')
+            async with aiofiles.open(f"category {i}/2001010{i}0{j}-{j}.md", "w") as f:
+                await f.write(f"post 2001010{i}0{j}-{j}.md")
+
 
 asyncio.run(main())
